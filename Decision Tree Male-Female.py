e408b910cc22c11bdf0ccbd413688e917267de71
Alex Fields <alx1056@hotmail.com>
2018-06-23 12:07:05 -0400

    Update Decision Tree Male-Female.py

--- Decision Tree Male-Female.py
+++ Decision Tree Male-Female.py
@@ -17,22 +17,3 @@ prediction = clf.predict([[150, 50,1]])
 print(prediction) 
 tree.export_graphviz(clf,
     out_file='tree1.dot')     
-
-
-
-
-#Tree with Graphviz
-#Iris Decision Tree
-from sklearn.datasets import load_iris
-from sklearn import tree
-from graphviz import Digraph
-
-
-clf = tree.DecisionTreeClassifier()  
-
-clf = tree.DecisionTreeClassifier()
-iris = load_iris()
-clf = clf.fit(iris.data, iris.target)
-tree.export_graphviz(clf,
-    out_file='tree.dot')  
-
