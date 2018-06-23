6db635b591abe98fc2e1c166063cb51f360992c4
Alex Fields <alx1056@hotmail.com>
2018-06-23 11:56:15 -0400

    Create Decision Tree Male-Female.py

---
+++ Decision Tree Male-Female.py
@@ -0,0 +1,38 @@
+#Man-Woman Decision Tree
+from sklearn import tree
+from graphviz import Digraph
+clf = tree.DecisionTreeClassifier() 
+
+#[height cm, hair-length cm, voice-pitch (0-low, 1-high)]                                             
+X = [ [180, 15,0],                                                              
+      [167, 42,1],                                                              
+      [136, 35,1],                                                              
+      [174, 15,0],                                                              
+      [141, 28,1]]                                                              
+
+Y = ['man', 'woman', 'woman', 'man', 'woman']
+
+clf = clf.fit(X, Y)                                                             
+prediction = clf.predict([[150, 50,1]])                                         
+print(prediction) 
+tree.export_graphviz(clf,
+    out_file='tree1.dot')     
+
+
+
+
+#Tree with Graphviz
+#Iris Decision Tree
+from sklearn.datasets import load_iris
+from sklearn import tree
+from graphviz import Digraph
+
+
+clf = tree.DecisionTreeClassifier()  
+
+clf = tree.DecisionTreeClassifier()
+iris = load_iris()
+clf = clf.fit(iris.data, iris.target)
+tree.export_graphviz(clf,
+    out_file='tree.dot')  
+
